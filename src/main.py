fe166e2727c5f60518069e616aebd6d30b9349f0
= <=>
2023-03-07 15:50:01 +0100

    Added some Commentary

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from rich import console
 from rich import print
 from rich import progress
 from rich import prompt
-import rich
+import rich # Install rich with "pip install rich"
 import os
 console = console.Console()
 
