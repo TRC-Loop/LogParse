418e004f014ff99506f355f6e029e95249579ad5
= <=>
2023-03-07 15:53:43 +0100

    added helper libraries

---
+++ src/helper.py
@@ -0,0 +1,8 @@
+# Basic Initilazation
+from rich import console
+from rich import print
+from rich import progress
+from rich import prompt
+import rich # Install rich with "pip install rich"
+import os
+console = console.Console()
