1115dafe3bef014aeef1bb4865b51b72f02e9e1a
= <=>
2023-03-07 15:47:54 +0100

    added main path getting user inout

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# LogParse
+# By EikDaMike and TRC-Loop
+# Licensed under MIT License
+
+# Basic Initilazation
+from rich import console
+from rich import print
+from rich import progress
+from rich import prompt
+import rich
+import os
+console = console.Console()
+
+# User Input for the path
+if prompt.Confirm("Do you want to scan all Logs in a specified path?", default=True):
+    path = prompt.Prompt("Enter the path to scan all the files", default=os.getcwd())
+else:
+    path = prompt.Prompt("Enter the path to scan one file", default=os.getcwd())
+
